01a3f113690ce1a8a99dee8f519ec5668bae4222
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-06-09 01:33:59 +0530

    Add Material You Theme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
+import MaterialYouThemeProvider from '@app/components/theme/MaterialYouThemeProvider';
 import RootNavigation from '@app/navigation/RootNavigation';
-import {PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   return (
-    <PaperProvider>
+    <MaterialYouThemeProvider>
       <SafeAreaProvider>
         <RootNavigation />
       </SafeAreaProvider>
-    </PaperProvider>
+    </MaterialYouThemeProvider>
   );
 }
 
