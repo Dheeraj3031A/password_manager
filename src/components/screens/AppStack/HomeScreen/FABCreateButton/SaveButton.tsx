a98dc16e867b1b65c73155b2fed312e090883f4a
RKS <ronitkrshah@tuta.io>
2024-06-10 21:50:37 +0530

    Don't save password if title or password is empty

--- src/components/screens/AppStack/HomeScreen/FABCreateButton/SaveButton.tsx
+++ src/components/screens/AppStack/HomeScreen/FABCreateButton/SaveButton.tsx
@@ -5,6 +5,7 @@ import {GlobalStoreRootState} from '@app/store/store';
 import {TPassword} from '@app/types/Password';
 import {Button} from 'react-native-paper';
 import {useDispatch, useSelector} from 'react-redux';
+import {ToastAndroid} from 'react-native';
 
 type Props = {
   data: TPassword;
@@ -18,6 +19,10 @@ function SaveButton({data, hideDialog}: Props) {
   const dispatch = useDispatch();
 
   function handleSave() {
+    if (!data.title || !data.password) {
+      ToastAndroid.show('Missing Fields!', ToastAndroid.SHORT);
+      return;
+    }
     hideDialog();
     dispatch(
       setPasswordsGlobalStore([...savedPasswords, {...data, id: nanoid()}]),
