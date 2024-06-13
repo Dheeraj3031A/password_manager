34e446fa8512e295963d1ac9708bedcd17afdfb2
RKS <ronitkrshah@tuta.io>
2024-06-13 22:25:31 +0530

    Show Dialog On File Importing

---
+++ src/components/shared/AppHeader/VerticalDotsMenu/Items/ImportFile/ConfirmationDialog.tsx
@@ -0,0 +1,93 @@
+import {Dispatch, SetStateAction} from 'react';
+import {Button, Dialog, Portal, Text} from 'react-native-paper';
+import {StyleSheet, ToastAndroid, View} from 'react-native';
+import {TItemProps} from '../..';
+import {TStoredPassword} from '@app/types/Password';
+import {removeDuplicatePasswords} from './utils/removeDuplicatePasswords';
+import {GlobalStoreRootState} from '@app/store/store';
+import {useDispatch, useSelector} from 'react-redux';
+import {setPasswordsGlobalStore} from '@app/store/features/passwordsSlice';
+
+type Props = {
+  visible: boolean;
+  setVisible: Dispatch<SetStateAction<boolean>>;
+  decryptedPasswords: TStoredPassword[];
+} & TItemProps;
+
+function ConfirmationDialog({
+  visible,
+  setVisible,
+  hideMenu,
+  decryptedPasswords,
+}: Props) {
+  const currentState = useSelector(
+    (state: GlobalStoreRootState) => state.passwords.passwords,
+  );
+  const dispatch = useDispatch();
+
+  function dismissDialog() {
+    setVisible(false);
+    hideMenu();
+  }
+
+  // TODO: Give a Better Function Name :)
+  function afterImport() {
+    dismissDialog();
+    hideMenu();
+    ToastAndroid.show('Importing Finished', ToastAndroid.SHORT);
+  }
+
+  function appendData() {
+    const {skipped, filteredArray} = removeDuplicatePasswords(
+      currentState,
+      decryptedPasswords,
+    );
+    dispatch(setPasswordsGlobalStore(filteredArray));
+    if (skipped > 0) {
+      ToastAndroid.show(`Duplicates Skipped ${skipped}`, ToastAndroid.SHORT);
+    }
+    afterImport();
+  }
+
+  function clearExistingPasswords() {
+    dispatch(setPasswordsGlobalStore(decryptedPasswords));
+    afterImport();
+  }
+
+  return (
+    <Portal>
+      <Dialog visible={visible} onDismiss={dismissDialog}>
+        <Dialog.Title>Select Method</Dialog.Title>
+        <Dialog.Content>
+          <Text variant="bodyMedium">
+            You have existing saved Passwords. How would you like to proceed
+            with importing new Passwords?
+          </Text>
+          <View style={styles.methodDesc}>
+            <Text>
+              <Text variant="titleMedium">Append Method: </Text>
+              Imported Passwords will be appended with current stored Passwords.
+            </Text>
+            <Text>
+              <Text variant="titleMedium">Clean: </Text>
+              Clear all current Passwords and save imported Passwords.
+            </Text>
+          </View>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={appendData}>Append</Button>
+          <Button onPress={clearExistingPasswords}>Clean</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
+}
+
+const styles = StyleSheet.create({
+  methodDesc: {
+    marginTop: 20,
+    gap: 10,
+  },
+});
+
+export default ConfirmationDialog;
--- src/components/shared/AppHeader/VerticalDotsMenu/Items/ImportFile/index.tsx
+++ src/components/shared/AppHeader/VerticalDotsMenu/Items/ImportFile/index.tsx
@@ -1,55 +1,45 @@
 import {Menu} from 'react-native-paper';
 import * as RNFS from '@dr.pogodin/react-native-fs';
 import {ToastAndroid} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
-import {setPasswordsGlobalStore} from '@app/store/features/passwordsSlice';
-import {decryptPasswordFromAESEncryption} from '@app/utils/crypto';
-import {GlobalStoreRootState} from '@app/store/store';
-import {removeDuplicatePasswords} from './utils/removeDuplicatePasswords';
 import {TItemProps} from '../..';
+import ConfirmationDialog from './ConfirmationDialog';
+import {useState} from 'react';
+import {decryptPasswordFromAESEncryption} from '@app/utils/crypto';
+import {TStoredPassword} from '@app/types/Password';
 
 function ImportFile({hideMenu}: TItemProps) {
-  const currentState = useSelector(
-    (state: GlobalStoreRootState) => state.passwords.passwords,
-  );
-  const dispatch = useDispatch();
+  const [showDialog, setShowDialog] = useState(false);
+  const [decryptedPasswords, setDecryptedPasswords] = useState<
+    TStoredPassword[]
+  >([]);
 
   async function handleImport() {
     try {
       ToastAndroid.show('Select Saved File', ToastAndroid.SHORT);
       const res = await RNFS.pickFile();
       const fileData = await RNFS.readFile(res[0]);
-
-      if (fileData) {
-        const decryptedData = decryptPasswordFromAESEncryption(fileData);
-        const {filteredArray, skipped} = removeDuplicatePasswords(
-          currentState,
-          decryptedData,
-        );
-        if (skipped > 0) {
-          ToastAndroid.show(
-            `Duplicates Skipped ${skipped}`,
-            ToastAndroid.SHORT,
-          );
-        }
-        dispatch(setPasswordsGlobalStore(filteredArray));
-      }
+      const data = decryptPasswordFromAESEncryption(fileData);
+      setDecryptedPasswords(data);
+      setShowDialog(true);
     } catch (e) {
       if (e instanceof Error) {
-        if (e.message === 'Malformed UTF-8 data') {
-          ToastAndroid.show('Inavalid Encryption Key!', ToastAndroid.SHORT);
-        } else {
-          ToastAndroid.show('Something Went Wrong', ToastAndroid.SHORT);
-        }
-      } else {
-        ToastAndroid.show('Something Went Wrong', ToastAndroid.SHORT);
+        ToastAndroid.show(`Error: ${e.message}`, ToastAndroid.SHORT);
       }
+      hideMenu();
     }
-
-    hideMenu();
   }
 
-  return <Menu.Item title="Import" onPress={handleImport} />;
+  return (
+    <>
+      <Menu.Item title="Import" onPress={handleImport} />
+      <ConfirmationDialog
+        decryptedPasswords={decryptedPasswords}
+        visible={showDialog}
+        setVisible={setShowDialog}
+        hideMenu={hideMenu}
+      />
+    </>
+  );
 }
 
 export default ImportFile;
